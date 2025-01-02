86a3abfd1f03cf1e0b047da6b016834662fa3dbd
buzanovsky <s.buzanovsky@yandex.ru>
2025-01-02 18:59:02 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = 'имя пользователя'
+title = 'заголовок заметки'
+content = 'описание заметки'
+status  = 'статус заметки'
+created_date = '01.01.2025'
+issue_date = '31.12.2025'
+
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки:", issue_date)
